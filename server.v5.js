44de2394edd083a02872517542d9376271f796a2
Marco Garcia <marcogarcias.2012@gmail.com>
2025-03-15 22:58:44 -0600

    Se agregan eventos para cuando el usuario con camara descativa camara, audio o sale de la sala.

--- server.v5.js
+++ server.v5.js
@@ -15,7 +15,13 @@ let roomNow;
 let users = {};
 
 io.on('connection', (socket) => {
-  socket.on('join', (roomId, userType) => {
+  socket.on('join', (data) => {
+    data = (typeof data === 'object') ? data : {};
+    let roomId = data.roomId ? data.roomId : null;
+    const userType = data.userType ? data.userType : null;
+    const nick = data.nick ? data.nick : socket.id;
+    const ip = data.ip ? data.ip : null;
+    console.log('1 sala', roomId);
     
     if(userType == 'kukurygirl' && !roomId){
       return socket.emit('socketErrores', {type: 'canceledJoin', userType: userType, message: 'Ingresa una sala.'});
@@ -35,7 +41,7 @@ io.on('connection', (socket) => {
     socket.join(roomId);
     roomNow = roomId;
 
-    users[socket.id] = {id: socket.id, name: '', type: 'admin', userType: userType};
+    users[socket.id] = {id: socket.id, nick: nick, ip: ip, userType: userType};
     const isBroadcaster = userType=='kukurygirl' || userType=='guest';
     let isFirstBroadcaster = false;
 
@@ -64,7 +70,7 @@ io.on('connection', (socket) => {
     }
 
     if(userType =='kukurygirl' || userType =='guest'){
-      console.log('1 join: userType', userType);
+      //console.log('1 join: userType', userType);
       if(userType =='guest'){
         socket.emit('set-room', {
           roomId: roomId,
@@ -82,7 +88,7 @@ io.on('connection', (socket) => {
 
       // Notify other broadcasters to connect with this broadcaster
       room.broadcasters.forEach(broadcasterId => {
-        console.log('2 join: broadcasterId para initiate-peer-connection', broadcasterId);
+        //console.log('2 join: broadcasterId para initiate-peer-connection', broadcasterId);
         if (broadcasterId !== socket.id) {
           io.to(broadcasterId).emit('initiate-peer-connection', socket.id, users);
         }
@@ -90,18 +96,18 @@ io.on('connection', (socket) => {
 
       // Notify all viewers about this broadcaster
       room.viewers.forEach(viewerId => {
-        console.log('Notifying viewer', viewerId, 'about broadcaster', socket.id);
+        //console.log('Notifying viewer', viewerId, 'about broadcaster', socket.id);
         io.to(viewerId).emit('broadcaster-joined', socket.id, users);
       });
     }
 
     if(userType =='viewer'){
-      console.log('3 join: userType', userType);
+      //console.log('3 join: userType', userType);
       socket.emit('set-room', {
         roomId: roomId,
         userType: userType
       });
-      
+
       socket.emit('broadcaster-status', {
         idUser: socket.id,
         roomId: roomId,
@@ -112,7 +118,7 @@ io.on('connection', (socket) => {
 
       // Notify broadcasters about the new viewer
       room.broadcasters.forEach(broadcasterId => {
-        console.log('4 join: broadcasterId para viewer-joined', broadcasterId);
+        //console.log('4 join: broadcasterId para viewer-joined', broadcasterId);
         io.to(broadcasterId).emit('viewer-joined', socket.id);
       });
     }
@@ -129,7 +135,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('offer', ({ targetId, offer }) => {
-    console.log(`Oferta de ${socket.id} para ${targetId}`);
+    //console.log(`Oferta de ${socket.id} para ${targetId}`);
     socket.to(targetId).emit('offer', {
       offer,
       offerId: socket.id
@@ -137,7 +143,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('answer', ({ targetId, answer }) => {
-    console.log(`Respuesta de ${socket.id} para ${targetId}`);
+    //console.log(`Respuesta de ${socket.id} para ${targetId}`);
     socket.to(targetId).emit('answer', {
       answer,
       answerId: socket.id
@@ -196,10 +202,12 @@ io.on('connection', (socket) => {
       // Determine if user is broadcaster or viewer
       const isBroadcaster = room.broadcasters.includes(socket.id);
       const userType = isBroadcaster ? 'Broadcaster' : 'Espectador';
+      const nick = users[socket.id].nick ? users[socket.id].nick : socket.id;
       
       // Create message object
       const messageData = {
         userId: socket.id.slice(0, 6),
+        nick, nick,
         userType: userType,
         message: message,
         timestamp: new Date().toISOString()
@@ -207,6 +215,67 @@ io.on('connection', (socket) => {
       
       // Send message to everyone in the room
       io.to(roomId).emit('chat-message', messageData);
+    }
+  });
+
+  // Manejar control de medios (video/audio)
+  socket.on('media-control', (data) => {
+    const roomId = data.roomId;
+    const room = rooms[roomId];
+    
+    if (room) {
+      // Añadir información del usuario que realizó el cambio
+      data.userId = socket.id;
+      data.nick = users[socket.id]?.nick || socket.id;
+      
+      // Reenviar a todos en la sala
+      io.to(roomId).emit('media-control', data);
+      console.log(`Usuario ${socket.id} (${data.nick}) cambió estado de ${data.mediaType} a ${data.enabled ? 'activado' : 'desactivado'}`);
+    }
+  });
+
+  // Manejar cuando un usuario abandona voluntariamente
+  socket.on('leave-room', (data) => {
+    const roomId = data.roomId;
+    const room = rooms[roomId];
+    
+    if(room){
+      // Notificar a todos los demás usuarios en la sala
+      socket.to(roomId).emit('user-left', socket.id);
+      
+      // Remover al usuario de la sala
+      const broadcasterIndex = room.broadcasters.indexOf(socket.id);
+      if(broadcasterIndex !== -1) {
+        room.broadcasters.splice(broadcasterIndex, 1);
+      }
+      
+      const viewerIndex = room.viewers.indexOf(socket.id);
+      if(viewerIndex !== -1) {
+        room.viewers.splice(viewerIndex, 1);
+      }
+      
+      // Si es kukurygirl y está saliendo, reset roomNow
+      if(users[socket.id] && users[socket.id].userType === 'kukurygirl') {
+        roomNow = null;
+      }
+      
+      // Abandonar la sala en Socket.IO
+      socket.leave(roomId);
+      
+      // Si la sala queda vacía, eliminarla
+      if(room.broadcasters.length === 0 && room.viewers.length === 0) {
+        delete rooms[roomId];
+      } else {
+        // Actualizar información de la sala para los restantes
+        io.to(roomId).emit('room-info', {
+          roomId,
+          broadcasters: room.broadcasters,
+          viewerCount: room.viewers.length,
+          users: users
+        });
+      }
+      
+      console.log(`Usuario ${socket.id} abandonó voluntariamente la sala ${roomId}`);
     }
   });
 });
